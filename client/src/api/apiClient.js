8a985dfc047f8b0f8e1a83195a2bff3f7a15ac39
MohamedAtheq <atheqsocial@gmail.com>
2026-05-23 22:52:51 +0530

    errors crcted

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://127.0.0.1:5000"  || "https://document-upload-20rg.onrender.com";
+  import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,21 +4,13 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css'
-import { Toaster }
-from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-
   <React.StrictMode>
-
-    <Toaster
-      position="top-right"
-    />
-
+    <Toaster position="top-right" />
     <App />
-
   </React.StrictMode>
-
 );
